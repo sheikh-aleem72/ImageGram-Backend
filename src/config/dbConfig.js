b48e69f91d00337554a1126b30e9af3894757dc6
aleem <sheikhaleem363@gmail.com>
2025-08-22 22:09:39 +0530

    fix: fixed mongoose connection logic

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,14 +1,10 @@
 import mongoose from 'mongoose';
 
-import { DEV_DB_URL, NODE_ENV, PROD_DB_URL } from './serverConfig.js';
+import { DEV_DB_URL, NODE_ENV } from './serverConfig.js';
 
 export default async function connectDB() {
   try {
-    if (NODE_ENV === 'production') {
-      await mongoose.connect(PROD_DB_URL);
-    } else if (NODE_ENV === 'development') {
-      await mongoose.connect(DEV_DB_URL);
-    }
+    await mongoose.connect(DEV_DB_URL);
 
     console.log(`Connected to ${NODE_ENV} database successfully`);
   } catch (error) {
